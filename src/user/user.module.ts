b2471550c0b9daec1a6e49e561d7c9536e97e609
Marcus Maurmann <marcuslma@gmail.com>
2024-07-03 12:15:41 -0300

    Organize imports

---
+++ src/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+import { UserService } from './user.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [UserService],
+  exports: [],
+})
+export class UserModule {}
