64862eb81f182addc10d6108405a29cbdb1eeaa4
Marcus Maurmann <marcuslma@gmail.com>
2024-07-03 18:13:04 -0300

    Add class-validator and class-transform to packages, add a global pipe validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minNumbers: 0,
+    minSymbols: 0,
+    minLowercase: 0,
+    minUppercase: 0,
+  })
+  password: string;
+}
